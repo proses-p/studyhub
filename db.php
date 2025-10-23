0c9788db78deeff37e6c889fb51fc91a64e6435b
Proses Projectus <prosesprojestus0@gmail.com>
2025-10-23 14:27:02 +0300

    Switch to environment variables for DB configuration

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'studyhub');
-define('DB_PORT', '3307');
+$DB_HOST = getenv('MYSQL_HOST') ?: '127.0.0.1';
+$DB_USER = getenv('MYSQL_USER') ?: 'root';
+$DB_PASS = getenv('MYSQL_PASSWORD') ?: '';
+$DB_NAME = getenv('MYSQL_DATABASE') ?: 'studyhub';
+$DB_PORT = getenv('MYSQL_PORT') ?: '3307';
 
 $dbconn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 if (!$dbconn) {
